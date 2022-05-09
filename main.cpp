cde02301dd77ac637b3c5a6f6a9f185e07b14204
lperson- <lperson-@student.42.fr>
2022-05-09 09:10:48 +0200

    FIX: catch exceptions when app starts

--- main.cpp
+++ main.cpp
@@ -3,15 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bbellavi <bbellavi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lperson- <lperson-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/22 15:02:17 by lperson-          #+#    #+#             */
-/*   Updated: 2022/04/22 15:45:13 by bbellavi         ###   ########.fr       */
+/*   Updated: 2022/05/09 09:10:24 by lperson-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cstdlib>
+#include <exception>
 #include "App.hpp"
 
 int main(int argc, char *argv[])
@@ -24,6 +25,10 @@ int main(int argc, char *argv[])
 	std::string pass = argv[2];
 	App app;
 
-	app.start(port, pass);
+	try {
+		app.start(port, pass);
+	} catch (std::exception const &e) {
+		std::cerr << e.what() << std::endl;
+	}
 	return EXIT_SUCCESS;
 }
