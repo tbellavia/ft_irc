1cb4b00fb3b85190306447169ca7c326cc25d8b9
lperson- <lperson-@student.42.fr>
2022-05-09 13:02:37 +0200

    FIX: compile flag for c++98

---
+++ srcs/irc/cmd/CmdMODE.cpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   CmdMODE.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lperson- <lperson-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/09 10:52:41 by lperson-          #+#    #+#             */
+/*   Updated: 2022/05/09 14:24:33 by lperson-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "irc/cmd/CmdMODE.hpp"
+#include <iostream>
+
+IRC::CmdMODE::CmdMODE(CmdCtx &ctx, std::string const &request):
+	ACmd(ctx, request, "MODE") {
+		std::cout << "HELLO" << std::endl;
+	}
+
+IRC::CmdMODE::~CmdMODE() { }
+
+IRC::Actions IRC::CmdMODE::execute() {
+	std::cout << "Commmand MODE:";
+
+	std::vector<std::string> args = this->parse();
+	ReplyBuilder reply(this->server_name(), this->sender());
+
+	if ( args.size() < Expected_args(1) )
+		return IRC::Actions::unique_send(
+			this->sender(), reply.error_need_more_params("MODE")
+		);
+	if ( IRC::Channel::is_channel_name(args[1]) )
+		return this->execute_channel_mode_(args, reply);
+
+	std::cout << std::endl;
+	return IRC::Actions::unique_idle();
+}
+
+IRC::Actions IRC::CmdMODE::execute_channel_mode_(
+	std::vector<std::string> const &args, ReplyBuilder &reply
+) {
+	Channel *channel = this->channels().find(args[1]);
+	if (!channel)
+		return IRC::Actions::unique_send(
+			this->sender(), reply.error_no_such_channel(args[1])
+		);
+
+	
+
+	return Actions::unique_idle();
+}
